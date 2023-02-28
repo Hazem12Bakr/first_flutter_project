8f1858bdd2fea81eede3ec8dfc5a70ea218f1a90
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-02-28 18:09:50 +0200

    creating the appbar icon

--- lib/views/widgets/custon_app_bar.dart
+++ lib/views/widgets/custon_app_bar.dart
@@ -6,12 +6,32 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
+      children: const [
         Text(
           'Notes',
-          style: TextStyle(),
-        )
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Center(
+        child: Icon(Icons.search),
+      ),
     );
   }
 }
