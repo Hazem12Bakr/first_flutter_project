a953add65e3adcfbea8bea06227586d9d975f9d8
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-02-28 17:48:56 +0200

    styling the appBar

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/widgets/custon_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: const [
+        SizedBox(
+          height: 50,
+        ),
+        CustomAppBar(),
+      ],
+    );
   }
 }
