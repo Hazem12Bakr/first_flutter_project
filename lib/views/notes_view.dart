9f179e948692cf796f0fcc04d9ad20b93a1e70e1
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-02-28 16:46:25 +0200

    creating appbar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NotesViewBody(),
+    );
+  }
+}
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
