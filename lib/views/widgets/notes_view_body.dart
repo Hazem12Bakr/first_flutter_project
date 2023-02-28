a32cd3667205d006cfd42beae629ad6c44b367a3
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-02-28 18:41:25 +0200

    styling the appbar icon

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/views/widgets/custon_app_bar.dart';
+import 'package:flutter_application_1/views/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
