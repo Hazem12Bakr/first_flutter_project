00d64bb017e0586b14c3a2cb9f473e8fa138bfac
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-01 23:26:42 +0200

    custonize the card note in a new file

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/widgets/custom_app_bar.dart';
+import 'package:flutter_application_1/views/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
@@ -15,65 +16,6 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomAppBar(),
           NoteItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCCB0),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Build your career with Hazem A.Bakr',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.6),
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'Mar 1 , 2023',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16,
-              ),
-            ),
-          ),
         ],
       ),
     );
