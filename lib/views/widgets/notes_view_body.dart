26667f0c33500854f95c4bf06df7b9fbe503aca2
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-01 23:08:55 +0200

    padding and resizing the card content

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -27,6 +27,7 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
       decoration: BoxDecoration(
         color: const Color(0xffFFCCB0),
         borderRadius: BorderRadius.circular(16),
@@ -42,25 +43,35 @@ class NoteItem extends StatelessWidget {
                 fontSize: 26,
               ),
             ),
-            subtitle: Text(
-              'Build your career with Hazem A.Bakr',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 20,
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+              ),
+              child: Text(
+                'Build your career with Hazem A.Bakr',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.6),
+                  fontSize: 18,
+                ),
               ),
             ),
             trailing: IconButton(
                 onPressed: () {},
-                icon: Icon(
+                icon: const Icon(
                   Icons.delete,
                   color: Colors.black,
-                  size: 30,
+                  size: 24,
                 )),
           ),
-          Text(
-            'Mar 1 , 2023',
-            style: TextStyle(
-              color: Colors.black.withOpacity(.4),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'Mar 1 , 2023',
+              style: TextStyle(
+                color: Colors.black.withOpacity(.4),
+                fontSize: 16,
+              ),
             ),
           ),
         ],
