54c2c1e0e2e5e032f7a4401a4a29afa45871cdf5
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-03 14:54:24 +0200

    a new file to handel the buttomsheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/views/widgets/add_note_buttomsheet.dart';
 import 'widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -11,27 +12,16 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
-          showModalBottomSheet(context: context, builder: (context) 
-          {
-            return  Container();
-          },)
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteButtomSheet();
+            },
+          );
         },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
-  }
-}
-
-
-class AddNoteButtomSheet extends StatelessWidget {
-  const AddNoteButtomSheet({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) 
-  {
-    return Container();
-
   }
 }
