213a2216bc31e44bfa73a74a163e927afba6b585
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-03 14:43:17 +0200

    create a buttomsheet in the add icon

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -10,10 +10,28 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+
+          showModalBottomSheet(context: context, builder: (context) 
+          {
+            return  Container();
+          },)
+        },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
+  }
+}
+
+
+class AddNoteButtomSheet extends StatelessWidget {
+  const AddNoteButtomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Container();
+
   }
 }
