c8a8d0cb221cfaedecb1e095f6d8ba2ba66f199f
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-05 17:38:28 +0200

    modify the appbar

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key, required this.title}) : super(key: key);
+  const CustomAppBar({Key? key, required this.title, required this.icon})
+      : super(key: key);
 
   final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: const [
+      children: [
         Text(
-          'Notes',
-          style: TextStyle(
+          title,
+          style: const TextStyle(
             fontSize: 28,
           ),
         ),
         Spacer(),
-        CustomSearchIcon(),
+        CustomSearchIcon(
+          icon: icon,
+        ),
       ],
     );
   }
