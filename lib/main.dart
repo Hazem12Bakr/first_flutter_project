74cde4d4dd75ebfade11c966ef852cc3b22acb94
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-05 23:49:01 +0200

    add hive DB to my project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/notes_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  //initialize hive
+  await Hive.initFlutter();
+
+  // open the box
+  var box = await Hive.openBox('mybox');
+
   runApp(const NotesApp());
 }
 
