a8026d301e93817de70e68b36bf9697f91bc2d83
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-05 17:32:03 +0200

    note view app bar icon

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({Key? key, required this.title}) : super(key: key);
 
+  final String title;
   @override
   Widget build(BuildContext context) {
     return Row(
