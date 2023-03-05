f10d91a10e7b6d8e6379454b372d5a8c61fe2bb7
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-05 17:06:36 +0200

    recal customappbar to create note appbar

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/widgets/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar()
+        ],
+      ),
+    );
   }
 }
