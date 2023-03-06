738a1d7dac4b056c18c03df5cbcced22771caccc
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-06 13:25:56 +0200

    generate notet model.g file (hive)

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
-  //initialize hive
-  // await Hive.initFlutter();
-
-  // open the box
-  //var box = await Hive.openBox('mybox');
-
+void main() {
   runApp(const NotesApp());
 }
 
