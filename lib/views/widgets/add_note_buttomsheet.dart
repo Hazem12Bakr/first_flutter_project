4c918f607ec77e4b11334dd932bb130d1969cae9
Hazem Abo Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-06 21:17:25 +0200

    add autovalidateMode to return ifenter wrong error

--- lib/views/widgets/add_note_buttomsheet.dart
+++ lib/views/widgets/add_note_buttomsheet.dart
@@ -8,33 +8,54 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 32,
-            ),
-            CustomtextField(
-              hint: 'title',
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomtextField(
-              hint: 'Content',
-              maxlines: 5,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 16,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 32,
+          ),
+          CustomtextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomtextField(
+            hint: 'Content',
+            maxlines: 5,
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          CustomButton(),
+          SizedBox(
+            height: 16,
+          ),
+        ],
       ),
     );
   }
